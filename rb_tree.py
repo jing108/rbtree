23398a1eb6ab49176de06a91c0d80bdf4062d5ef
jing107 <xiaochenking@qq.com>
2017-11-29 23:55:13 +0800

    完成红黑树的插入操作，并进行了简单测试。 等待更多测试，看是否有bug

--- rb_tree.py
+++ rb_tree.py
@@ -37,15 +37,104 @@ class RBTree(object):
         p = node.parent
         if p.parent.left == p:
             while p:
-                if p.parent.right and (not p.parent.is_black):
+                if p.parent.right and (not p.parent.right.is_black):
                     # case 1
                     p.is_black = True
                     p.parent.is_black = False
                     p.parent.right.is_black = True
-                    if p.parent
-                elif node == parent
+                    if (not p.parent.parent) or p.parent.parent.is_black:
+                        break
+                    else:
+                        p = p.parent.parent
+                elif node == p.left:
+                    # case 2: 如果node的叔叔节点存在且为黑色或者其叔叔节点不存在，node节点为父节点的左子节点
+                    self.__rb_right_rotate(node.parent)
+                    break
+                else:
+                    # case 3: 如果node的叔叔节点存在且为黑色或者不存在，node节点为父节点的右子节点
+                    self.__left_rotate(node.parent)
+                    self.__rb_right_rotate(node)
+                    break
         else:
-            pass
+            while p:
+                if p.parent.left and (not p.parent.left.is_black):
+                    # case 1
+                    p.is_black = True
+                    p.parent.is_black = False
+                    p.parent.left.is_black = True
+                    if (not p.parent.parent) or p.parent.parent.is_black:
+                        break
+                    else:
+                        p = p.parent.parent
+                elif node == p.right:
+                    self.__rb_left_rotate(node.parent)
+                    break
+                else:
+                    self.__right_rotate(node.parent)
+                    self.__rb_left_rotate(node)
+                    break
+
+        self.__root.is_black = True
+
+    @staticmethod
+    def __right_rotate(node):
+        node.parent.right = node.left
+        node.left.parent = node.parent
+
+        node.parent = node.left
+        node.left.right = node
+        node.left = None
+
+    @staticmethod
+    def __left_rotate(node):
+        node.parent.left = node.right
+        node.right.parent = node.parent
+
+        node.parent = node.right
+        node.right.left = node
+        node.right = None
+
+    def __rb_right_rotate(self, node):
+        p = node.parent
+
+        node.parent = p.parent
+        if p.parent:
+            if p.parent.left == p.parent:
+                p.parent.left = node
+            else:
+                p.parent.right = node
+        else:
+            self.__root = node
+
+        p.parent = node
+        p.left = node.right
+        if node.right:
+            node.right.parent = p
+        node.right = p
+
+        node.is_black = True
+        p.is_black = False
+
+    def __rb_left_rotate(self, node):
+        p = node.parent
+
+        node.parent = p.parent
+        if p.parent:
+            if p.parent.left == p.parent:
+                p.parent.left = node
+            else:
+                p.parent.right = node
+        else:
+            self.__root = node
+
+        p.parent = node
+        p.right = node.left
+        if node.left:
+            node.left.parent = p
+        node.left = p
+
+        node.is_black = True
+        p.is_black = False
 
     def insert(self, value):
         node = self.__root
@@ -66,9 +155,5 @@ class RBTree(object):
 
 if __name__ == '__main__':
     rb_tree = RBTree()
-    rb_tree.insert(4)
-    rb_tree.insert(3)
-    rb_tree.insert(4)
-    rb_tree.insert(5)
-    rb_tree.insert(2)
-    rb_tree.insert(8)
+    for i in [10, 40, 30, 60, 90, 70, 20, 50, 80]:
+        rb_tree.insert(i)
